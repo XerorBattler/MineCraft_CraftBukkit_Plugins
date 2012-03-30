42ef05b72cba7db58b9b5c828e5a672cd8490dfd
VaclavHaramule <xerorbattler@gmail.com>
2012-03-30 12:54:41 +0200

    enum fix

--- SignEnchanter/src/net/XerorBattler/SignEnchanter/SEEnchantList.java
+++ SignEnchanter/src/net/XerorBattler/SignEnchanter/SEEnchantList.java
@@ -1,7 +1,7 @@
 package net.XerorBattler.SignEnchanter;
 
 public enum SEEnchantList {
-    Durability ("DURABILITY");
+    DURABILITY ("Durability");
 
     private final String name;
 
@@ -14,11 +14,11 @@ public enum SEEnchantList {
         return this.name;
     }
     
-   static public boolean isMember(String name) {
+   static public String getMember(String name) {
        SEEnchantList[] enchants = SEEnchantList.values();
        for (SEEnchantList enchant : enchants)
-           if (enchant.name.equals(name))
-               return true;
-       return false;
+           if (enchant.name.equalsIgnoreCase(name))
+               return enchant.name();
+       return null;
    }
 }
